a1df57e005fb382c44995bd85b450ebf923b285f
yi-yun <yi--yun@outlook.com>
2019-02-03 19:25:21 +0800

    add binary

--- LeetCode/LeetCode_287.py
+++ LeetCode/LeetCode_287.py
@@ -4,10 +4,17 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
+        length = len(nums)-1
+        for i in range(length):
+            while i != nums[i]:
+                if nums[i] == nums[nums[i]]:
+                    return nums[i]
+                # 交换nums[i]和nums[nums[i]]两个的位置
+                # temp = nums[nums[i]]
+                # nums[nums[i]] = nums[i]
+                # nums[i] = temp
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
 
-
-# return (sum(nums)-sum(set(nums)))//(len(nums) - len(set(nums)))
 
 a = [1, 3, 4, 2, 2]
 print(Solution().findDuplicate(a))
--- LeetCode/LeetCode_852.py
+++ LeetCode/LeetCode_852.py
@@ -1,22 +1,36 @@
 class Solution:
+    # def peakIndexInMountainArray(self, A):
+    #     """
+    #     :type A: List[int]
+    #     :rtype: int
+    #     """
+    #     low = 0
+    #     higt = len(A)-1
+    #     while low < higt:
+    #         med = (low + higt) // 2
+    #         if med == len(A)-1:
+    #             return len(A)-1
+    #         elif A[med-1] < A[med] and A[med] > A[med+1]:
+    #             return med
+    #         elif A[med-1] > A[med]:
+    #             higt = med - 1
+    #         else:
+    #             low = med + 1
+    #     return low
+
     def peakIndexInMountainArray(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        low = 0
-        higt = len(A)-1
-        while low < higt:
-            med = (low + higt) // 2
-            if med == len(A)-1:
-                return len(A)-1
-            elif A[med-1] < A[med] and A[med] > A[med+1]:
-                return med
-            elif A[med-1] > A[med]:
-                higt = med - 1
+        low, high = 0, len(A) - 1
+        while low <= high:
+            mid = (high - low) // 2 + low
+            if mid in (0, len(A) - 1):
+                return mid
+            elif A[mid] > A[mid - 1] and A[mid] > A[mid + 1]:
+                return mid
+            elif A[mid] < A[mid - 1]:
+                high = mid - 1
             else:
-                low = med + 1
-        return low
+                low = mid + 1
+        return -1
 
 
 print(Solution().peakIndexInMountainArray([1, 2, 3, 2, 1]))
