f5d5c0a1c7b7a889381fbbe28584aa70df517fa8
yi-yun <yi--yun@outlook.com>
2019-02-23 21:33:51 +0800

    add del_node.py

---
+++ offer/deteleNode.py
@@ -0,0 +1,16 @@
+# need confirm exist
+def delete_node(link, node):
+    # 只有一个节点
+    if link == node:
+        del node
+    # tail node
+    if node.next is None:
+        while link:
+            if link.next == node:
+                link.next = None
+            link = link.next
+    else:  # 中间节点
+        temp = node.next
+        node.val = temp.val
+        node.next = temp.next
+        del temp
