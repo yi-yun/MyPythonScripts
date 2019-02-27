a24b0d1a2fa962aa9cc47070d2435105b6435f88
yi-yun <yi--yun@outlook.com>
2019-02-27 19:21:36 +0800

    add bfs_tree2

--- offer/bfs_tree.py
+++ offer/bfs_tree.py
@@ -12,3 +12,27 @@ def bfs(tree):
         if node.right:
             queue.append(node.right)
     return ret
+
+
+# print Tree Node with \n
+def bfs_change(tree):
+    if not tree:
+        return None
+    queue = [tree]
+    count = 1  # to print
+    nextLevel = 0  # level
+    ret = []
+    while queue:
+        node = queue.pop(0)
+        while count:
+            print(node.val)
+            count -= 1
+        ret.append(node.val)
+        nextLevel = 1 if node.left or node.right else 0
+        if nextLevel == 1:
+            print("\n")
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return ret
