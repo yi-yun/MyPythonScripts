d0c425e7bd344b1cc298f3e7fcd68d3dc5502f61
yi-yun <yi--yun@outlook.com>
2019-03-23 18:10:57 +0800

    add tree iner

--- BinNode.py
+++ BinNode.py
@@ -1,12 +1,36 @@
-class BinNode():
-    def __init__(self, val):
-        self.lchild = None
-        self.rchild = None
-        self.val = val
+"""
+    1
+   / \
+  2   3
+ / \   \
+4   5   6
+- 层次遍历顺序：[1 2 3 4 5 6]
+- 前序遍历顺序：[1 2 4 5 3 6]
+- 中序遍历顺序：[4 2 5 1 3 6]
+- 后序遍历顺序：[4 5 2 6 3 1]
+"""
 
+
+class BinNode():
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+"""
+    1
+   / \
+  2   3
+ / \   \
+4   5   6
+- 前序遍历顺序：[1 2 4 5 3 6]
+"""
 
 # 非递归
-def preOrder(self, root):
+
+
+def preOrder1(root):
     if not root:
         return
     myStack = []
@@ -15,12 +39,12 @@ def preOrder(self, root):
         while node:
             print(node.val)
             myStack.append(node)
-            node = node.lchild
+            node = node.left
         node = myStack.pop()
-        node = node.rchild
+        node = node.right
 
 
-def preorderTraversal(root):
+def preOrder2(root):
     """
     :type root: TreeNode
     :rtype: List[int]
@@ -38,3 +62,30 @@ def preorderTraversal(root):
             stack.append(top.left)
         result.append(top.val)
     return result
+
+
+"""
+    1
+   / \
+  2   3
+ / \   \
+4   5   6
+- 中序遍历顺序：[4 2 5 1 3 6]
+"""
+
+
+def inOrder(root):  # 中序
+    res = []
+    stack = []
+    if root is None:
+        return res
+    cur = root
+    while len(stack) != 0 or cur is not None:
+        while cur is not None:
+            stack.append(cur)
+            cur = cur.left
+        node = stack.pop()
+        res.append(node.val)
+        if node.right:
+            cur = node.right
+    return res
