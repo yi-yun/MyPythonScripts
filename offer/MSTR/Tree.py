f5fc23fdc9220557b823855cac2c1dfa62ff6038
yi-yun <yi--yun@outlook.com>
2019-04-11 11:59:46 +0800

    add mstr

--- offer/MSTR/Tree.py
+++ offer/MSTR/Tree.py
@@ -9,3 +9,11 @@ class BSTreeNode:
 def isPresent(root, val):
     # write your code here
     # return 1 or 0 depending on whether the element is present in the tree or not
+    temp = root
+    while temp:
+        if val == temp.val:
+            return temp
+        if val > temp.value:
+            temp = temp.right
+        else:
+            temp = temp.left
