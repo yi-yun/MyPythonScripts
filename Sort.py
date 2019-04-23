b362163110b7612c62e25c625259636ea273eb2b
yi-yun <yi--yun@outlook.com>
2019-04-23 15:04:25 +0800

    add n^2 sort

--- Sort.py
+++ Sort.py
@@ -12,5 +12,29 @@ def insert(source):
     return source
 
 
+def bubblesort(source):
+    # add flag to fast
+    for i in range(len(source)):
+        for j in range(1, len(source)-i):
+            if source[j] < source[j - 1]:
+                source[j], source[j - 1] = source[j - 1], source[j]
+    return source
+
+
+def selectionsort(source):
+    for i in range(0, len(source)):
+        min = source[i]
+        index = i
+        for j in range(i, len(source)):
+            if source[j] < min:
+                min = source[j]
+                index = j
+        source[i], source[index] = source[index], source[i]
+    return source
+
+
 if __name__ == "__main__":
-    print(insert([4, 5, 6, 1, 3, 2]))
+    test_list = [4, 5, 6, 1, 3, 2]
+    # print(insert(test_list))
+    # print(bubblesort(test_list))
+    print(selectionsort(test_list))
