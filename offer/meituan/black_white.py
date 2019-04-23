6c44a2e0c576257cd281d1607671d0b6a569a20e
yi-yun <yi--yun@outlook.com>
2019-04-23 22:26:37 +0800

    add correct meituan

--- offer/meituan/black_white.py
+++ offer/meituan/black_white.py
@@ -1,5 +1,3 @@
-
-
 if __name__ == "__main__":
     m, n = map(int, input().strip().split())
     source = []
@@ -29,11 +27,24 @@ if __name__ == "__main__":
         list1.append(i)
     for i in dic_2.items():
         list2.append(i)
-    # print(list1, list2)
+    print(list1, list2)
     a = list1[0]
     b = list2[0]
     # if a[0] != b[0]:
     #     print(m*n//2+1-a[1]+m*n//2-b[1])
     if a[0] == b[0]:
-        b = list2[1]
-    print(m * n - a[1] - b[1])
+        if len(list2) > 1:
+            b = list2[1]
+            left = m * n - a[1] - b[1]
+        else:
+            left = m * n - a[1]
+        if len(list1) > 1:
+            a = list1[1]
+            right = m*n-a[1]-b[1]
+        else:
+            right = m * n - b[1]
+        print(left) if left < right else print(right)
+    else:
+        print(m * n - a[1] - b[1])
+    # print(b)
+    # print(m * n - a[1] - b[1])
