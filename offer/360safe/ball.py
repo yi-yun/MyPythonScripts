3e5b7261ecf057c6c10dd48ccfabe99f5b155fdf
yi-yun <yi--yun@outlook.com>
2019-04-25 19:23:08 +0800

    format

--- offer/360safe/ball.py
+++ offer/360safe/ball.py
@@ -1,7 +1,9 @@
-def gcd(a,b):
+def gcd(a, b):
     while b:
-        a,b=b,a%b
+        a, b = b, a % b
     return a
+
+
 def test(dic):
     # for i in dic.values():
     #     if i < 1:
@@ -10,28 +12,30 @@ def test(dic):
     list1 = []
     for i in dic.values():
         list1.append(i)
-    if len(list1)==1:
+    if len(list1) == 1:
         return 1
-    tmp=list1[0]
+    tmp = list1[0]
 
-    cnt=0
-    a=gcd(tmp,list1[1])
+    cnt = 0
+    a = gcd(tmp, list1[1])
     for i in list1:
         # if a==1:
         #     return 0
-        a=gcd(a,i)
-        if a==1:
+        a = gcd(a, i)
+        if a == 1:
             return 0
         else:
-            cnt+=i//a
+            cnt += i//a
     return cnt
+
+
 if __name__ == "__main__":
     tcase = int(input().strip())
     data = list(map(int, input().strip().split()))
-    dic={}
+    dic = {}
     for i in data:
         if i not in dic.keys():
-            dic[i]=data.count(i)
+            dic[i] = data.count(i)
     print(test(dic))
     # a=gcd(5,5)
     # print(a)
